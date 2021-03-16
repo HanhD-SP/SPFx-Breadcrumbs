4ace25ea277affde86d77a1763105499e16b8174
Hanh <hanh.duong@octoconsulting.com>
2021-03-16 13:06:10 -0700

    added rootUrl and routing

---
+++ src/extensions/siteBreadcrumb/components/SiteBreadcrumb.module.scss.ts
@@ -0,0 +1,10 @@
+/* tslint:disable */
+require("./SiteBreadcrumb.module.css");
+const styles = {
+  breadcrumb: 'breadcrumb_123a2405',
+  msBgColorThemePrimary: 'msBgColorThemePrimary_123a2405',
+  breadcrumbLinks: 'breadcrumbLinks_123a2405'
+};
+
+export default styles;
+/* tslint:enable */
